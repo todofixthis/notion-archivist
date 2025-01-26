87ac4820ba7f4bd88a03d00c5e816b09393c4c00
Phoenix Zerin <todofixthis@users.noreply.github.com>
2025-01-27 09:11:11 +1300

    Stubbed out markdown parser tests ✏️

--- src/services/notion/markdownParser.test.ts
+++ src/services/notion/markdownParser.test.ts
@@ -2,7 +2,39 @@ import { BlockObjectRequest } from "@notionhq/client/build/src/api-endpoints";
 import { describe, expect, it } from "vitest";
 import MarkdownParser from "./markdownParser";
 
+/**
+ * @see https://github.com/markedjs/marked/blob/master/test/unit/Lexer.test.js
+ */
 describe("MarkdownParser Text Formatting", () => {
+  describe.skip("Blockquotes", () => {
+    // Placeholder
+  });
+
+  describe.skip("Line breaks", () => {
+    // Placeholder
+  });
+
+  describe.skip("Code", () => {
+    describe("Fenced/indented code block", () => {
+      // Placeholder
+
+      it("Passes through whitespace-only code blocks", () => {
+        // Placeholder
+        // Note: the Marked library has special logic for different cases of leading and
+        // trailing whitespace. Refer to its unit tests for more information (the link
+        // is at the top of this file).
+      });
+    });
+
+    describe("Code span", () => {
+      // Placeholder
+
+      it("Passes through whitespace-only code spans", () => {
+        // Placeholder
+      });
+    });
+  });
+
   describe("Basic Text and Paragraphs", () => {
     it("converts a simple paragraph", () => {
       const markdown = "This is a simple paragraph";
@@ -22,8 +54,8 @@ describe("MarkdownParser Text Formatting", () => {
         },
       ];
 
-      const result = MarkdownParser.blocksFromMarkdown(markdown);
-      expect(result).toEqual(expected);
+      const actual = MarkdownParser.blocksFromMarkdown(markdown);
+      expect(actual).toEqual(expected);
     });
 
     it("converts multiple paragraphs into separate blocks", () => {
@@ -56,24 +88,40 @@ describe("MarkdownParser Text Formatting", () => {
         },
       ];
 
-      const result = MarkdownParser.blocksFromMarkdown(markdown);
-      expect(result).toEqual(expected);
+      const actual = MarkdownParser.blocksFromMarkdown(markdown);
+      expect(actual).toEqual(expected);
+    });
+
+    it.skip("Handles bold-formatted text", () => {
+      // Placeholder
+    });
+
+    it.skip("Handles italic-formatted text", () => {
+      // Placeholder
+    });
+
+    it.skip("Handles strikethrough-formatted text", () => {
+      // Placeholder
     });
 
     it("is a no-op for empty input", () => {
       const markdown = "";
       const expected: BlockObjectRequest[] = [];
 
-      const result = MarkdownParser.blocksFromMarkdown(markdown);
-      expect(result).toEqual(expected);
+      const actual = MarkdownParser.blocksFromMarkdown(markdown);
+      expect(actual).toEqual(expected);
     });
 
     it("is a no-op for whitespace-only input", () => {
       const markdown = "   \n   \n   ";
       const expected: BlockObjectRequest[] = [];
 
-      const result = MarkdownParser.blocksFromMarkdown(markdown);
-      expect(result).toEqual(expected);
+      const actual = MarkdownParser.blocksFromMarkdown(markdown);
+      expect(actual).toEqual(expected);
+    });
+
+    it.skip("Treats escaped tokens as plain text", () => {
+      // Placeholder
     });
   });
 
@@ -98,24 +146,24 @@ describe("MarkdownParser Text Formatting", () => {
             } as BlockObjectRequest,
           ];
 
-          const result = MarkdownParser.blocksFromMarkdown(markdown);
-          expect(result).toEqual(expected);
+          const actual = MarkdownParser.blocksFromMarkdown(markdown);
+          expect(actual).toEqual(expected);
         });
 
         it("ignores the heading if empty", () => {
           const markdown = `${"#".repeat(level)} `;
           const expected: BlockObjectRequest[] = [];
 
-          const result = MarkdownParser.blocksFromMarkdown(markdown);
-          expect(result).toEqual(expected);
+          const actual = MarkdownParser.blocksFromMarkdown(markdown);
+          expect(actual).toEqual(expected);
         });
 
         it("ignores the heading if whitespace-only", () => {
           const markdown = `${"#".repeat(level)}   \t  `;
           const expected: BlockObjectRequest[] = [];
 
-          const result = MarkdownParser.blocksFromMarkdown(markdown);
-          expect(result).toEqual(expected);
+          const actual = MarkdownParser.blocksFromMarkdown(markdown);
+          expect(actual).toEqual(expected);
         });
       },
     );
@@ -141,26 +189,94 @@ describe("MarkdownParser Text Formatting", () => {
             },
           ];
 
-          const result = MarkdownParser.blocksFromMarkdown(markdown);
-          expect(result).toEqual(expected);
+          const actual = MarkdownParser.blocksFromMarkdown(markdown);
+          expect(actual).toEqual(expected);
         });
 
         it("ignores the heading if empty", () => {
           const markdown = `${"#".repeat(level)} `;
           const expected: BlockObjectRequest[] = [];
 
-          const result = MarkdownParser.blocksFromMarkdown(markdown);
-          expect(result).toEqual(expected);
+          const actual = MarkdownParser.blocksFromMarkdown(markdown);
+          expect(actual).toEqual(expected);
         });
 
         it("ignores the heading if whitespace-only", () => {
           const markdown = `${"#".repeat(level)}   \t  `;
           const expected: BlockObjectRequest[] = [];
 
-          const result = MarkdownParser.blocksFromMarkdown(markdown);
-          expect(result).toEqual(expected);
+          const actual = MarkdownParser.blocksFromMarkdown(markdown);
+          expect(actual).toEqual(expected);
         });
       },
     );
+  });
+
+  describe.skip("Horizontal rule", () => {
+    // Placeholder
+  });
+
+  describe.skip("HTML", () => {
+    // Placeholder
+  });
+
+  describe.skip("Hyperlinks", () => {
+    describe("Bare links", () => {
+      // Placeholder `url` and `<url>`
+    });
+
+    describe("Inline links", () => {
+      // Placeholder `[title](url)`
+    });
+
+    describe("Named links", () => {
+      // Placeholder `[name]` with URL set in page footer ("Def" and "Link" tokens)
+
+      it("Does its best if the link reference is invalid", () => {
+        // Placeholder (what should it do for e.g. `[link]` where there's no def for `link`?)
+      });
+    });
+  });
+
+  describe.skip("Images", () => {
+    // Placeholder
+  });
+
+  describe.skip("Lists", () => {
+    describe("Checklists", () => {
+      // Placeholder
+
+      describe("Blocks within list items", () => {
+        // Placeholder
+      });
+    });
+
+    describe("Ordered lists", () => {
+      // Placeholder
+
+      describe("Blocks within list items", () => {
+        // Placeholder
+      });
+    });
+
+    describe("Unordered lists", () => {
+      // Placeholder
+
+      describe("Blocks within list items", () => {
+        // Placeholder
+      });
+    });
+  });
+
+  describe.skip("Preformatted text", () => {
+    // Placeholder
+  });
+
+  describe.skip("Tables", () => {
+    // Placeholder
+  });
+
+  describe.skip("Whitespace", () => {
+    // Placeholder
   });
 });
